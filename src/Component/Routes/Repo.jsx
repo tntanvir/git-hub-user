22c9c6565611c5d8f67a08dbf5d74877f0b5e0a3
Tanvir <tntanvir2382018@gmail.com>
2024-01-12 22:49:18 +0600

    git repo edit

--- src/Component/Routes/Repo.jsx
+++ src/Component/Routes/Repo.jsx
@@ -13,16 +13,51 @@ const Repo = () => {
             .then(res => res.json())
             .then(data => setRepo(data));
     }, [])
-    console.log(repo);
+    const len = (jj) => {
+
+        return jj.length > 100 ? jj.slice(0, 100) + "...." : jj;
+    }
+
+    // console.log(repo);
+    const languageColors = {
+        "JavaScript": "bg-yellow-400",
+        "C++": "bg-red-500",
+        "Python": "bg-blue-400",
+        "Java": "bg-blue-600",
+        "Swift": "bg-orange-600",
+        "Ruby": "bg-pink-600",
+        "HTML": "bg-red-600",
+        "CSS": "bg-teal-600",
+        "TypeScript": "bg-blue-700",
+        "Go": "bg-cyan-600",
+        "C#": "bg-darkblue-600",
+        "Kotlin": "bg-indigo-600",
+        "Rust": "bg-red-300",
+        "Elixir": "bg-violet-600",
+        "Haskell": "bg-violet-900",
+    }
+
     return (
-        <div className='flex flex-col gap-4 px-5'>
+        <div className='flex flex-wrap bg- gap-4 px-5 '>
             {
                 repo && repo.map((e) => (
-                    <div key={e.id} className='dark:bg-gray-900 bg-gray-200 min-h-24'>
+                    <div key={e.id} className='dark:bg-gray-900 bg-gray-200 min-h-24 w-96 p-3 rounded-md'>
                         <h1 className='text-2xl text-blue-700'>{e.name}</h1>
-                        <p className='text-sm '>{e.description}</p>
+                        {
+                            e.description && <p className='text-sm' >{len(e.description)}</p>
+                        }
                         <a href={e.homepage} className='hover:text-blue-700'>{e.homepage}</a>
-                        <p>{e.language}</p>
+                        {e.language &&
+                            <div className='flex gap-2 items-center '>
+                                <p className={`h-2 w-2 rounded-full  ${languageColors[e.language]}`}></p>
+                                <p className={``}>{e.language}</p>
+
+                            </div>
+
+
+                        }
+
+
                     </div>
                 )
                 )
